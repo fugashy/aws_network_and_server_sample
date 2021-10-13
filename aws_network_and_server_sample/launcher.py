9f250a4585e3e7316efe084382896c53dc3b1f7f
fugashy <fugashy@icloud.com>
2021-10-13 23:49:51 +0900

    Modify to create route table for public subnet

--- aws_network_and_server_sample/launcher.py
+++ aws_network_and_server_sample/launcher.py
@@ -94,3 +94,68 @@ class InternetGateWayLauncher():
         self._client.delete_internet_gateway(
             InternetGatewayId=self.id)
         print(f"delete {self._conf['name']}")
+
+
+class RouteTableLauncher():
+    def __init__(self, conf):
+        self._conf = conf
+
+        self._client = boto3.client('ec2')
+        self.id = None
+        self._association_id = None
+
+    def run(self, attach_vpc_id, *, attach_subnet_id=None):
+        # VPCにルータを置くイメージ
+        res = self._client.create_route_table(
+            VpcId=attach_vpc_id,
+            TagSpecifications=[
+                {
+                    'ResourceType': 'route-table',
+                    'Tags': [
+                        {
+                            'Key': 'Name',
+                            'Value': self._conf['name']}]}])
+        self.id = res['RouteTable']['RouteTableId']
+
+        if attach_subnet_id is not None:
+            res = self._client.associate_route_table(
+                RouteTableId=self.id,
+                SubnetId=attach_subnet_id)
+            self._association_id = res['AssociationId']
+        print(f"create {self._conf['name']}")
+
+    def kill(self):
+        if self.id is None:
+            return
+
+        if self._association_id is not None:
+            self._client.disassociate_route_table(
+                AssociationId=self._association_id)
+
+        self._client.delete_route_table(RouteTableId=self.id)
+        print(f"delete {self._conf['name']}")
+
+
+class RouteLauncher():
+    def __init__(self, conf):
+        self._conf = conf
+
+        self._client = boto3.client('ec2')
+        self.id = None
+        self._rt_id = None
+
+    def run(self, attach_rt_id, *, attach_igw_id=None):
+        # igwへのトラフィックを設定し，テーブルに登録
+        self._client.create_route(
+            DestinationCidrBlock=self._conf['DestinationCidrBlock'],
+            GatewayId=attach_igw_id,
+            RouteTableId=attach_rt_id)
+        self._rt_id = attach_rt_id
+
+    def kill(self):
+        if self.id is None:
+            return
+
+        self._client.delete_route(
+            DestinationCidrBlock=self._conf['DestinationCidrBlock'],
+            RouteTableId=self._rt_id)
--- aws_network_and_server_sample/main.py
+++ aws_network_and_server_sample/main.py
@@ -5,6 +5,8 @@ import yaml
 
 from launcher import (
     InternetGateWayLauncher,
+    RouteLauncher,
+    RouteTableLauncher,
     SubnetLauncher,
     VpcLauncher,
 )
@@ -19,17 +21,32 @@ def main(config_file):
     vpc_array = [VpcLauncher(c) for c in config['vpc']]
     subnet_array = [SubnetLauncher(c) for c in config['subnet']]
     igw_array = [InternetGateWayLauncher(c) for c in config['internet_gateway']]
+    rt_array = [RouteTableLauncher(c) for c in config['route_table']]
+    route_array = [RouteLauncher(c) for c in config['route']]
 
     try:
+        # TODO(fugashy) この辺のデータ受け渡し方法は微妙だが，
+        # - AWSの仕様を完全に把握していないので下手に抽象化などは避けた
+        # - 本の内容を掴むには十分
+        # なのでしばらくこのまま
         [vpc.run() for vpc in vpc_array]
         vpc_id = vpc_array[0].id
         [subnet.run(vpc_id) for subnet in subnet_array]
+        # publicな方はルートテーブルを設定するのでID確保
+        public_subnet_id = subnet_array[0].id
         [igw.run(vpc_id) for igw in igw_array]
+        # IGWのIDはルートテーブル設定に必要なので確保
+        igw_id = igw_array[0].id
+        [rt.run(vpc_id, attach_subnet_id=public_subnet_id) for rt in rt_array]
+        rt_id = rt_array[0].id
+        [route.run(rt_id, attach_igw_id=igw_id) for route in route_array]
     except Exception as e:
         print(f'Error: {e}')
 
     input('Enter to terminate instances')
 
+    [route.kill() for route in reversed(route_array)]
+    [rt.kill() for rt in reversed(rt_array)]
     [igw.kill() for igw in reversed(igw_array)]
     [subnet.kill() for subnet in reversed(subnet_array)]
     [vpc.kill() for vpc in reversed(vpc_array)]
